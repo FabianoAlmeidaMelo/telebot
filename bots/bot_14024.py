ce43fadc3ce558ad3f8b627f422f802133a5d7d8
Fabiano Almeida <fabiano@jetbov.com>
2022-06-23 15:17:16 -0300

    NOvas consultas

--- bots/bot_14024.py
+++ bots/bot_14024.py
@@ -4,22 +4,49 @@ import logging
 
 from config import API_KEY
 from views import (
-    eagle_saldo
+    eagle_saldo,
+    eagle_pesos
 )
-
+from config import JETBOV_TOKEN, phone_numbers
 bot = telebot.TeleBot(API_KEY)
 
 logger = telebot.logger
 
-phone_numbers = {
-    '5512982239764': True,
-    '5512997164534': True
-}
+autenticated = {}
+
+def cant_access(message):
+    msg = 'Entre em contato com o administrador da sua Fazenda, para liberar seu acesso'
+    bot.send_message(
+        message.chat.id,
+        msg
+    )
 
 
 @bot.message_handler(commands=["saldo"])
 def get_saldo(message):
-    msg = eagle_saldo(32086)
+    farm_id = token = None
+    farm_id = autenticated.get(message.from_user.id)['farm_id']
+    token = autenticated.get(message.from_user.id)['token']
+
+    if farm_id and token:
+        msg = eagle_saldo(farm_id, token)
+    else:
+        msg: 'Não consegui identificar sua fazenda'
+    bot.send_message(
+        message.chat.id,
+        msg
+    )
+
+@bot.message_handler(commands=["prx_pgtos"])
+def get_prx_pgtos(message):
+    farm_id = token = None
+    farm_id = autenticated.get(message.from_user.id)['farm_id']
+    token = autenticated.get(message.from_user.id)['token']
+
+    if farm_id and token:
+        msg = eagle_prx_pgtos(farm_id, token)
+    else:
+        msg: 'Não consegui identificar sua fazenda'
     bot.send_message(
         message.chat.id,
         msg
@@ -35,7 +62,7 @@ def get_finance_options(message):
     /prx_recebimentos: 10 próximos recebimentos em Aberto
     /pgtos_vencidos: Valor total dos Pagamentos vencidos em Aberto
     /recb_vencidos: Valor total dos Recebimentos vencidos em Aberto
-    /initial: Volta para o menu inicial
+    /inicial: Volta para o menu inicial
     Clique em uma das opções
     """.format(first_name=message.from_user.first_name)
     bot.reply_to(message, menu)
@@ -47,19 +74,34 @@ def get_storage_options(message):
     /sem_saldo: Items sem saldos no estoque
     /abx_min: Items com saldo abaixo do mínimo
     /all_in: Todos items do estoque
-    /initial: Volta para o menu inicial
+    /inicial: Volta para o menu inicial
     Clique em uma das opções
     """
     bot.reply_to(message, menu)
 
 
+@bot.message_handler(commands=["pesos"])
+def get_pesos(message):
+    farm_id = token = None
+    farm_id = autenticated.get(message.from_user.id)['farm_id']
+    token = autenticated.get(message.from_user.id)['token']
+
+    if farm_id and token:
+        msg = eagle_pesos(farm_id, token)
+    else:
+        msg: 'Não consegui identificar sua fazenda'
+    bot.send_message(
+        message.chat.id,
+        msg
+    )
+
 @bot.message_handler(commands=["lotes"])
 def get_lotes_options(message):
     menu = """
     Sobre o que você quer saber sobre seus Lotes:
-    /pesos: Peso dos Lotes
-    /valor: Estimativa de valor na cotação de hoje
-    /initial: Volta para o menu inicial
+    /pesos: Peso e cotação dos Lotes
+    /ultima_pesagem: Dta da Última pesagem
+    /inicial: Volta para o menu inicial
     Clique em uma das opções
     """
     bot.reply_to(message, menu)
@@ -76,13 +118,13 @@ def get_app_options(message):
     /aprovadas: Total de atividades Aprovadas nesse mês
     /reprovaddas: Total de atividades Reprovadas nesse mês
     /pendentes: Total de atividades pendentes nesse mês
-    /initial: Volta para o menu inicial
+    /inicial: Volta para o menu inicial
     """
     bot.reply_to(message, menu)
 
 
-@bot.message_handler(commands=["initial"])
-def get_initial_menu(message):
+@bot.message_handler(commands=["inicial"])
+def get_inicial_menu(message):
     menu = """
     {first_name} aqui está o nosso menu inicial:
     /financeiro: Informações financeiras
@@ -96,21 +138,38 @@ def get_initial_menu(message):
         menu
     )
 
-autentcated = {}
 
 @bot.message_handler(content_types=["contact"])
 def contact_handler(message):
-    if not autentcated.get(message.contact.user_id):
-        autentcated[message.contact.user_id] = message.contact.phone_number
-    if autentcated.get(message.contact.user_id):
-        get_initial_menu(message)
+    habilitado = bool(phone_numbers.get(message.contact.phone_number))
+    if not autenticated.get(message.contact.user_id):
+        if habilitado:
+            autenticated[message.contact.user_id] = {
+                'phone': message.contact.phone_number,
+                'chat_id': message.contact.user_id,
+                'farm_id': phone_numbers.get(message.contact.phone_number)['farm_id'],
+                'token': phone_numbers.get(message.contact.phone_number)['token']
+            }
+        else:
+            cant_access(message)
+    if autenticated.get(message.contact.user_id) and habilitado:
+        get_inicial_menu(message)
 
 
 def autenticar(message):
-    if message.contact and autentcated.get(message.from_user.id) is None:
-        autentcated[message.contact.user_id] = message.contact.phone_number
-        return True
-    elif autentcated.get(message.from_user.id):
+    if message.contact and autenticated.get(message.from_user.id) is None:
+        if phone_numbers.get(message.contact.phone_number):
+            autenticated[message.contact.user_id] = {
+                'phone': message.contact.phone_number,
+                'chat_id': message.contact.user_id,
+                'farm_id': phone_numbers.get(message.contact.phone_number)['farm_id'],
+                'token': phone_numbers.get(message.contact.phone_number)['token']
+            }
+            return True
+        else:
+            cant_access(message)
+            return False
+    elif autenticated.get(message.from_user.id):
         return True
     else:
         return True
@@ -118,7 +177,7 @@ def autenticar(message):
 
 @bot.message_handler(func=autenticar)
 def start(message):
-    if autentcated.get(message.chat.id) is None:
+    if autenticated.get(message.chat.id) is None:
         markup = types.ReplyKeyboardMarkup(
             row_width=1,
             resize_keyboard=True
@@ -133,6 +192,13 @@ def start(message):
             'Seu telefone faz a autenticação',
             reply_markup=markup
         )
+    elif phone_numbers.get(autenticated.get(message.from_user.id)['phone']) is None:
+        msg = 'Entre em contato com o administrador da sua Fazenda, para liberar seu acesso'
+        bot.send_message(
+            message.chat.id,
+            msg
+        )
+
     else:
         menu = """
         Como posso te ajudar:
--- bots/views.py
+++ bots/views.py
@@ -1,21 +1,66 @@
 import json
 import requests
+import locale
 from datetime import datetime
-from config import JETBOV_TOKEN
+
 url = 'http://192.168.100.33:8008'
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
-
-def eagle_saldo(farm_id):
-    global url # 32086
-    url += f'/transaction/?farm_id={farm_id}'
-    saldo = requests.get(url, headers={'Authorization': f'Bearer {JETBOV_TOKEN}'})
+#financeiro
+def eagle_saldo(farm_id, token):
+    global url
+    url += f'/transaction/balance/?farm_id={farm_id}'
+    contas = requests.get(url, headers={'Authorization': f'Bearer {token}'})
+    print('-----------`o/------------------')
+    print(contas.json())
     print('-----------------------------')
+    data = datetime.today().strftime("%d/%m/%Y")
+
+    msg = f'{data}\n'
+    msg += 'Conta   saldo\n'
+    msg += '--------------------------------------------\n' 
+    for conta in contas.json():
+        name = conta.get('account_name')
+        valor = round(conta.get('balance'), 2)
+        valor = locale.currency(valor, grouping=True, symbol=None)
+        valor = f'R$ {valor}'
+        msg += f'{name}  {valor}\n\n'
+
+    return msg
+
+#financeiro
+def eagle_prx_pgtos(farm_id, token):
+    global url
+    url += f'/transaction/?farm_id={farm_id}'
+    saldo = requests.get(url, headers={'Authorization': f'Bearer {token}'})
+    print('--------****----------------')
     print(saldo.json()[0])
     print('-----------------------------')
     data = datetime.today().strftime("%d/%m/%Y")
-    msg = ''
-    for conta in ['Itaú', 'C6 Bank']:
-        msg += f'{data} - Conta: {conta}, saldo: R$ 1520,00 {saldo}\n'
 
     return str(saldo.json()[0])
 
+#lotes
+def eagle_pesos(farm_id, token):
+    global url
+    url += f'/herd/?farm_id={farm_id}'
+    lotes = requests.get(url, headers={'Authorization': f'Bearer {token}'})
+    print('--------****----------------')
+    print(lotes.json())
+    print('-----------------------------')
+
+    cotacao = lotes.json()[0].get('day_quote')
+    data = datetime.today().strftime("%d/%m/%Y")
+    msg = f'@ BOI ESALQ/BMF ({data}):  R$ {cotacao}\n\n'
+    msg += 'Nome   (Nr animais)  peso total   Cotação\n'
+    msg += '--------------------------------------------\n' 
+    for lote in lotes.json():
+        name = lote.get('name')
+        animais = str(lote.get('animalCount'))
+        peso = lote.get('total_weight')
+        valor = round(lote.get('quote'), 2)
+        valor = locale.currency(valor, grouping=True, symbol=None)
+        valor = f'R$ {valor}'
+        msg += f'{name}  ({animais})\n {peso}Kg {valor}\n\n'
+
+    return msg
